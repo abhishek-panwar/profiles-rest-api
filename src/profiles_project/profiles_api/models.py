a3c61253f0317eb0dd744b88668649713bb15d8e
unknown <panwarabhishek89@gmail.com>
2018-05-21 13:05:34 -0400

    added new model for sstoring profile feed

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -56,3 +56,25 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """django uses its to convert an object to string."""
 
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str___(self):
+        """return the model as a string"""
+
+        return self.status_text
+
+
+
+
+
+
+
+
+#
